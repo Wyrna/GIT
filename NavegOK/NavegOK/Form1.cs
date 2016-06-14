bd0597521f872531f22b31ca1bfe01af69a4bc48
Wyrna <m.jesus.patricio@hotmail.com>
2016-06-14 12:41:29 +0100

    Add files via upload

--- NavegOK/NavegOK/Form1.cs
+++ NavegOK/NavegOK/Form1.cs
@@ -86,6 +86,30 @@ namespace NavegOK
         {
 
         }
-        
+        private void SuppressScriptErrorsOnly(WebBrowser browser)
+        {
+            // Ensure that ScriptErrorsSuppressed is set to false.
+            browser.ScriptErrorsSuppressed = false;
+
+            // Handle DocumentCompleted to gain access to the Document object.
+            browser.DocumentCompleted +=
+                new WebBrowserDocumentCompletedEventHandler(
+                    browser_DocumentCompleted);
+        }
+
+        private void browser_DocumentCompleted(object sender,
+            WebBrowserDocumentCompletedEventArgs e)
+        {
+            ((WebBrowser)sender).Document.Window.Error +=
+                new HtmlElementErrorEventHandler(Window_Error);
+        }
+
+        private void Window_Error(object sender,
+            HtmlElementErrorEventArgs e)
+        {
+            // Ignore the error and suppress the error dialog box. 
+            e.Handled = true;
+        }
+
     }
 }
